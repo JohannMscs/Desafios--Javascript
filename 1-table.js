84f98879915d8a36e3ee8958568ef378f36001bf
Johann Moscoso <110245986+JohannMscs@users.noreply.github.com>
2023-07-14 19:34:51 -0300

    Tabuada
    tabuada de 6 só com os resultados e tabuada de 10 completa

---
+++ 1-table.js
@@ -0,0 +1,22 @@
+//Resultados da tabuada de 6
+function result(n){
+    for(n = 1; n<=10; n++)
+    console.log(+ n *6);
+}
+result(console.log("Resultados da tabuada de 6"))
+//Tabuada de 10
+function table(){
+let n10 = 10;
+console.log(`10 x 1 = ${n10*1}`)
+console.log(`10 x 2 = ${n10*2}`)
+console.log(`10 x 3 = ${n10*3}`)
+console.log(`10 x 4 = ${n10*4}`)
+console.log(`10 x 5 = ${n10*5}`)
+console.log(`10 x 6 = ${n10*6}`)
+console.log(`10 x 7 = ${n10*7}`)
+console.log(`10 x 8 = ${n10*8}`)
+console.log(`10 x 9 = ${n10*9}`)
+console.log(`10 x 10 = ${n10*10}`)
+}
+table(console.log("Tabuada de 10"))
+
